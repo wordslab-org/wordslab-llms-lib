7ea1bc606bc9168b1941a126ef463acc8aa2afb9
laurentprudhon <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>
2024-02-11 11:50:07 +0000

    Run nbdev_export at least once

--- wordslab_llms/_modidx.py
+++ wordslab_llms/_modidx.py
@@ -4,5 +4,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_baseurl': '/wordslab-llms-lib',
                 'doc_host': 'https://wordslab-org.github.io',
                 'git_url': 'https://github.com/wordslab-org/wordslab-llms-lib',
-                'lib_path': 'wordslab_llms_lib'},
-  'syms': {'wordslab_llms_lib.core': {'wordslab_llms_lib.core.foo': ('core.html#foo', 'wordslab_llms_lib/core.py')}}}
+                'lib_path': 'wordslab_llms'},
+  'syms': {'wordslab_llms.core': {'wordslab_llms.core.foo': ('core.html#foo', 'wordslab_llms/core.py')}}}
